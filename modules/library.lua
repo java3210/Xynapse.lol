8d355d0b8920ef2e439e4096d4be8abb4f2e2ce3
java3210 <phongsakon00912za@gmail.com>
2025-08-14 02:28:37 +0700

    Update library.lua

--- modules/library.lua
+++ modules/library.lua
@@ -1559,7 +1559,7 @@ function Compkiller:_AddLinkValue(Name , Default , GlobalBlock , LinkValues , re
 		ToggleValue.Name = Compkiller:_RandomString()
 		ToggleValue.Parent = Toggle
 		ToggleValue.AnchorPoint = Vector2.new(0.5, 0.5)
-		ToggleValue.BackgroundColor3 = Color3.new(0, 0, 0)
+		ToggleValue.BackgroundColor3 = Color3.new(1, 1, 1)
 		ToggleValue.BorderColor3 = Color3.fromRGB(0, 0, 0)
 		ToggleValue.BorderSizePixel = 0
 		ToggleValue.Position = UDim2.new(0.25, 0, 0.5, 0)
@@ -1615,11 +1615,9 @@ function Compkiller:_AddLinkValue(Name , Default , GlobalBlock , LinkValues , re
 		local ToggleUI = function(bool)
 			if bool then
 				ToggleElement(Default,true);
-
-				Compkiller:_Animation(ToggleValue,rep.Tween,{
-					BackgroundTransparency = 0
-				})
-
+				
+				ToggleValue.BackgroundColor3 = Color3.new(0, 0, 0)
+				
 				Compkiller:_Animation(Toggle,rep.Tween,{
 					BackgroundTransparency = 0
 				})
@@ -1627,12 +1625,12 @@ function Compkiller:_AddLinkValue(Name , Default , GlobalBlock , LinkValues , re
 				Compkiller:_Animation(UIStroke,rep.Tween,{
 					Transparency = 0
 				})
+				
+				ToggleValue.BackgroundColor3 = Color3.new(0, 0, 0)
 			else
 				ToggleElement(false,true);
 
-				Compkiller:_Animation(ToggleValue,rep.Tween,{
-					BackgroundTransparency = 1
-				})
+				ToggleValue.BackgroundColor3 = Color3.new(1, 1, 1)
 
 				Compkiller:_Animation(Toggle,rep.Tween,{
 					BackgroundTransparency = 1
